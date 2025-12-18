1158dd3d7ba1fa5f86d84f4c37f3b0f4b4e87e3d
Brian Robinson <brian@ndmweb.com>
2025-12-18 09:23:32 -0700

    Build in actions

--- forge.config.js
+++ forge.config.js
@@ -13,22 +13,22 @@ module.exports = {
   },
   rebuildConfig: {},
   makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
+    // {
+    //   name: '@electron-forge/maker-squirrel',
+    //   config: {},
+    // },
     {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin', 'win32'],
     },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    // {
+    //   name: '@electron-forge/maker-deb',
+    //   config: {},
+    // },
+    // {
+    //   name: '@electron-forge/maker-rpm',
+    //   config: {},
+    // },
   ],
   plugins: [
     {
